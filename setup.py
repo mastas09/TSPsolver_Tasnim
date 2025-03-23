5d38ce4fa990f0f2d6ebe3c293e96d53956a1098
Masrura Tasnim <161784186+mastas09@users.noreply.github.com>
2025-03-23 18:12:22 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='tsp_solver_Tasnim',
     version='0.1.0',
-    packages=['tsp_solver', 'tsp_solver.algorithms'],
+    packages=['tsp_solver_Tasnim', 'tsp_solver_Tasnim.algorithms'],
     description='A simple TSP solver library.',
     author='Your Name',
     author_email='your_email@example.com',
-    url='https://github.com/yourusername/TSPsolver',
+    url='https://github.com/yourusername/TSPsolver_Tasnim',
     license='MIT',
     install_requires=[],
     python_requires='>=3.6',
