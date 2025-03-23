278a8219bf1a7c20f82b7116616f9321c3151051
Masrura Tasnim <161784186+mastas09@users.noreply.github.com>
2025-03-23 17:05:50 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tsp_solver',
+    version='0.1.0',
+    packages=['tsp_solver', 'tsp_solver.algorithms'],
+    description='A simple TSP solver library.',
+    author='Your Name',
+    author_email='your_email@example.com',
+    url='https://github.com/yourusername/TSPsolver',
+    license='MIT',
+    install_requires=[],
+    python_requires='>=3.6',
+)
