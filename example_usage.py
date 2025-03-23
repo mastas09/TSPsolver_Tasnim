a302565c84bbbddcf19ec7baf4d41092abbe46de
Masrura Tasnim <161784186+mastas09@users.noreply.github.com>
2025-03-23 17:05:22 -0400

    Create example_usage.py

---
+++ example_usage.py
@@ -0,0 +1,7 @@
+from tsp_solver import random_search, hill_climbing, simulated_annealing
+
+cities = [(0, 0), (1, 1), (2, 2), (3, 3)]
+
+print("Random Search:", random_search(cities, iterations=1000))
+print("Hill Climbing:", hill_climbing(cities))
+print("Simulated Annealing:", simulated_annealing(cities, temp=1000, cooling_rate=0.995))
