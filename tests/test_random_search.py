e97aa36f1349ae17cbb78780ec552153c9a0dc47
Masrura Tasnim <161784186+mastas09@users.noreply.github.com>
2025-03-23 17:39:11 -0400

    Delete tests directory

--- tests/test_random_search.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from tsp_solver.algorithms.random_search import solve_tsp
-
-def test_random_search():
-    cities = [(0, 0), (1, 1), (2, 2)]
-    route, distance = solve_tsp(cities, iterations=100)
-    assert len(route) == len(cities)
-    assert distance > 0
